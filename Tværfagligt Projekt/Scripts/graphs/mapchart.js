6369dce6c370b90822fcbf2121f0a89ca7b2fc3b
nicolaims <you@example.com>
2023-12-09 16:07:01 +0100

    arbejder på verdenskortet på index

--- Tværfagligt Projekt/Scripts/graphs/mapchart.js
+++ Tværfagligt Projekt/Scripts/graphs/mapchart.js
@@ -1,65 +1,41 @@
-// Wrap your code in a document ready function
-document.addEventListener("DOMContentLoaded", function () {
-  // Example usage
-  const svgContainerSelector = "#chart-container"; // Use the selector for your SVG container
-  const styling = { width: 800, height: 500, marginLeft: 10, marginBottom: 10, marginRight: 10, marginTop: 10 }; // Adjust dimensions as needed
-  createMapChart(null, svgContainerSelector, styling);
-});
+let width = 1600;
+let height = 503;
 
-function createMapChart(data, svgSelector, styling) {
-  // Convert svg to D3 selection if not already
-  const svg = d3.select(svgSelector);
+// Select the map container and append an SVG element
+const svg = d3
+  .select(".map-container") // Use ".map-container" to select by class
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-  // Destructure styling object
-  const { width, height, marginLeft, marginBottom, marginRight, marginTop } =
-    styling;
+// Fetch world map data
+d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(
+  (data) => {
+    console.log("World map data:", data);
 
-  // Fetch world map data
-  d3.json(
-    "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json"
-  ).then((worldData) => {
     // Check if world map data is valid
-    if (worldData && worldData.objects && worldData.objects.countries) {
+    if (data && data.objects && data.objects.countries) {
       // Convert raw data to features
-      const countries = topojson.feature(
-        worldData,
-        worldData.objects.countries
-      );
+      const countries = topojson.feature(data, data.objects.countries);
+      console.log("Processed countries data:", countries);
 
-      // Create a projection
+      // Set up projection and path
       const projection = d3.geoMercator().fitSize([width, height], countries);
-
-      // Create a path generator
       const path = d3.geoPath().projection(projection);
 
-      // Remove previous paths
-      svg.selectAll("path").remove();
+      // Append a group element to the SVG
+      const g = svg.append("g");
 
-      // Append a path for each country
-      svg
-        .selectAll("path")
+      // Remove existing paths
+      g.selectAll("path").remove();
+
+      // Append new paths based on the country features
+      g.selectAll("path")
         .data(countries.features)
-        .join("path") // Use join instead of enter() to handle both enter and update selections
-        .attr("d", path)
-        .attr("fill", "lightgray")
-        .attr("stroke", "white")
-        .attr("stroke-width", 0.5);
-
-      // Add styling or additional features as needed
-      // ...
-
-      // Log information for debugging
-      console.log("World Map Data:", worldData);
-      console.log("Countries Features:", countries);
-      console.log("SVG Container:", svg.node());
-      console.log("SVG Width:", width);
-      console.log("SVG Height:", height);
+        .enter()
+        .append("path")
+        .attr("class", "country")
+        .attr("d", path);
     }
-  });
-}
-
-// Example usage
-const svgContainerSelector = "#chart-container"; // Use the selector for your SVG container
-
-}; // Adjust dimensions as needed
-createMapChart(null, svgContainerSelector, styling); // Pass null for data parameter since it's not used in the function
+  }
+);
