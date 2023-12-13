0062149516c2457c10aafc4e2d4f30ca5c997db0
user.name like levinuet <user.email like eaa23leej@students.eaaa.dk>
2023-12-13 09:18:57 +0100

    små ændringer

--- api/api.js
+++ api/api.js
@@ -63,7 +63,6 @@ app.get("/brazilfires", async (req, res) => {
   }
 });
 app.listen(port, () => {
-  console.log(`Server kører på http://localhost:${port}`);
   app.get("/pasturepercentage", async (req, res) => {
     try {
       let queryData = await klient.query("SELECT * FROM smallchange");
